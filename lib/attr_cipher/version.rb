0fb705d75df4935e54a8353d7b5071ff88ee2ff7
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2017-04-25 17:55:21 +1000

    Update gemspec description.

--- lib/attr_cipher/version.rb
+++ lib/attr_cipher/version.rb
@@ -1,11 +1,11 @@
 module AttrCipher
   module VERSION
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
     PRE   = nil
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     SUMMARY = "AttrCipher v#{STRING}"
-    DESCRIPTION = "Provides functionality to store encrypted attributes using AES-256-CBC."
+    DESCRIPTION = "Provides to transparently store encrypted attributes in ActiveRecord models."
   end
 end
